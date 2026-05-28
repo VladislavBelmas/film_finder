12753b2545c265cc3883a8e9a7d4dd4bc8be6f72
VladislavBelmas <vladislav.belmas1@gmail.com>
2026-05-28 16:25:15 +0200

    add 2 magic methods for "with"

--- db/mysql_client.py
+++ db/mysql_client.py
@@ -7,6 +7,14 @@ class MySql:
         self.connection = pymysql.connect(**config)
 
 
+    def __enter__(self):
+        return self
+
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+
     def select(self, query, params=None):
         """
         Выполняет SQL запрос.
