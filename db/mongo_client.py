d65d483352df44ddd6b07c0be69ecfcceac21ebe
VladislavBelmas <vladislav.belmas1@gmail.com>
2026-05-29 10:24:34 +0200

    add logger decorator to mongo_client.py

---
+++ db/mongo_client.py
@@ -0,0 +1,31 @@
+from pymongo import MongoClient
+from film_logger import logger_decorator
+
+class MongoDB:
+    @logger_decorator
+    def __init__(self, config):
+        self.client = MongoClient(config["uri"])
+        self.db = self.client[config["database"]]
+        self.collection = self.db[config["collection"]]
+
+
+    def __enter__(self):
+        return self
+
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    @logger_decorator
+    def ping(self):
+        return self.client.admin.command("ping")
+
+
+    @logger_decorator
+    def close(self):
+        self.client.close()
+
+
+    @logger_decorator
+    def log(self, doc):
+        return self.collection.insert_one(doc)
