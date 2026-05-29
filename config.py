2e0421c62609c9552e62f575592097dcb46751dd
VladislavBelmas <vladislav.belmas1@gmail.com>
2026-05-29 10:23:38 +0200

    set config.py

--- config.py
+++ config.py
@@ -1,11 +1,22 @@
 import os
 from dotenv import load_dotenv
+import pymysql
+
 
 load_dotenv()
 
-DB_CONFIG = {
-    "host": os.getenv("DB_HOST"),
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-    "database": os.getenv("DB_NAME"),
+MYSQL_CONFIG = {
+    "host": os.getenv("MYSQL_HOST"),
+    "user": os.getenv("MYSQL_USER"),
+    "password": os.getenv("MYSQL_PASSWORD"),
+    "database": os.getenv("MYSQL_NAME"),
+    'cursorclass': pymysql.cursors.DictCursor
 }
+
+
+MONGO_CONFIG = {
+    "uri": os.getenv("MONGO_URI"),
+    "database": os.getenv("MONGO_DB"),
+    "collection": os.getenv("MONGO_COLLECTION")
+}
+
