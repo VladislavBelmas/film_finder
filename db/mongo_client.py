6f1f38a3f1c94fee2c7da2b69f26cb618bdab743
VladislavBelmas <vladislav.belmas1@gmail.com>
2026-06-01 12:19:09 +0200

    add get_logs method and connection check

--- db/mongo_client.py
+++ db/mongo_client.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 from film_logger import logger_decorator
 
 class MongoDB:
     @logger_decorator
     def __init__(self, config):
-        self.client = MongoClient(config["uri"])
+        self.client = MongoClient(config["uri"], serverSelectionTimeoutMS=2000)
         self.db = self.client[config["database"]]
         self.collection = self.db[config["collection"]]
 
@@ -14,11 +15,16 @@ class MongoDB:
 
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.close()
+        if self.is_connected():
+            self.close()
 
 
-    def ping(self):
-        return self.client.admin.command("ping")
+    def is_connected(self):
+        try:
+            self._ping()
+            return True
+        except ConnectionFailure:
+            return False
 
 
     @logger_decorator
@@ -29,3 +35,13 @@ class MongoDB:
     @logger_decorator
     def log(self, doc):
         return self.collection.insert_one(doc)
+
+    @logger_decorator
+    def get_logs(self, filter_query=None, limit=100):
+        if filter_query is None:
+            filter_query = {}
+        return list(self.collection.find(filter_query).limit(limit).sort("_id", -1))
+
+
+    def _ping(self):
+        return self.client.admin.command("ping")
