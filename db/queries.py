5460182aa920487caa862153eba6e59c8a08f806
VladislavBelmas <vladislav.belmas1@gmail.com>
2026-06-03 00:35:09 +0200

    add films_by_category query

--- db/queries.py
+++ db/queries.py
@@ -5,7 +5,8 @@ MIN_FILM_YEAR = """SELECT MIN(release_year) AS min_year FROM film"""
 MAX_FILM_YEAR = """SELECT MAX(release_year) AS max_year FROM film"""
 
 FILMS = """
-        SELECT f.film_id, f.title, f.description, f.release_year, GROUP_CONCAT(c.name SEPARATOR ', ') as categories, f.rating
+        SELECT f.film_id, f.title, f.description, f.release_year, GROUP_CONCAT(c.name SEPARATOR ', ') as categories, 
+               f.rating
         FROM film f
         JOIN film_category fc
         ON f.film_id = fc.film_id
@@ -18,7 +19,8 @@ FILMS = """
         """
 
 FILMS_SPECIFIC_YEAR  = """
-                       SELECT f.film_id, f.title, f.description, f.release_year, GROUP_CONCAT(c.name SEPARATOR ', ') as categories, f.rating
+                       SELECT f.film_id, f.title, f.description, f.release_year, 
+                              GROUP_CONCAT(c.name SEPARATOR ', ') as categories, f.rating
                        FROM film f
                        JOIN film_category fc
                        ON f.film_id = fc.film_id
@@ -31,14 +33,20 @@ FILMS_SPECIFIC_YEAR  = """
                        """
 
 FILMS_BY_CATEGORY = """
-                    
-
-
-            
+                    SELECT f.film_id, f.title, f.description, f.release_year, 
+                           GROUP_CONCAT(c.name SEPARATOR ', ') as categories, f.rating
+                    FROM film f
+                    JOIN film_category fc
+                    ON f.film_id = fc.film_id
+                    JOIN category c 
+                    ON fc.category_id = c.category_id
+                    WHERE f.title LIKE %s AND f.release_year BETWEEN %s AND %s
+                    GROUP BY f.film_id
+                    HAVING FIND_IN_SET(%s, categories) > 0
+                    ORDER BY f.title
+                    LIMIT %s OFFSET %s     
                     """
 
 FILMS_BY_ACTOR = """
-
-
 
                  """
