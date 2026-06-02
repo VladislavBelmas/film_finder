664a8c8f3cbf5663a9b65330f57257f1af5ca8ca
VladislavBelmas <vladislav.belmas1@gmail.com>
2026-06-03 01:23:39 +0200

    add full main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from config import MYSQL_CONFIG, MONGO_CONFIG
+from db.mongo_client import MongoDB
+from db.mysql_client import MySql
+from repositories.movie_repository import MovieRepository
+from ui import Menu
+
+
+def main():
+    with MySql(MYSQL_CONFIG) as mysql, MongoDB(MONGO_CONFIG) as mongo:
+        repo = MovieRepository(mysql, mongo)
+        menu = Menu(repo)
+        menu.run()
+
+
+if __name__ == "__main__":
+    main()
