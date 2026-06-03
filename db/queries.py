c1cd2507d1d3e4e140b29a8d0c3e954a279d2832
VladislavBelmas <vladislav.belmas1@gmail.com>
2026-06-04 00:43:08 +0200

    add query

--- db/queries.py
+++ db/queries.py
@@ -19,34 +19,35 @@ FILMS = """
         """
 
 FILMS_SPECIFIC_YEAR  = """
-                       SELECT f.film_id, f.title, f.description, f.release_year, 
-                              GROUP_CONCAT(c.name SEPARATOR ', ') as categories, f.rating
-                       FROM film f
-                       JOIN film_category fc
-                       ON f.film_id = fc.film_id
-                       JOIN category c 
-                       ON fc.category_id = c.category_id
-                       WHERE f.title LIKE %s AND f.release_year = %s 
-                       GROUP BY f.film_id 
-                       ORDER BY f.title
-                       LIMIT %s OFFSET %s
+        SELECT f.film_id, f.title, f.description, f.release_year, 
+            GROUP_CONCAT(c.name SEPARATOR ', ') as categories, f.rating
+        FROM film f
+        JOIN film_category fc
+        ON f.film_id = fc.film_id
+        JOIN category c 
+        ON fc.category_id = c.category_id
+        WHERE f.title LIKE %s AND f.release_year = %s 
+        GROUP BY f.film_id 
+        ORDER BY f.title
+        LIMIT %s OFFSET %s
                        """
 
 FILMS_BY_CATEGORY = """
-                    SELECT f.film_id, f.title, f.description, f.release_year, 
-                           GROUP_CONCAT(c.name SEPARATOR ', ') as categories, f.rating
-                    FROM film f
-                    JOIN film_category fc
-                    ON f.film_id = fc.film_id
-                    JOIN category c 
-                    ON fc.category_id = c.category_id
-                    WHERE f.title LIKE %s AND f.release_year BETWEEN %s AND %s
-                    GROUP BY f.film_id
-                    HAVING FIND_IN_SET(%s, categories) > 0
-                    ORDER BY f.title
-                    LIMIT %s OFFSET %s     
+        SELECT f.film_id, f.title, f.description, f.release_year, 
+        GROUP_CONCAT(c.name SEPARATOR ', ') as categories, f.rating
+        FROM film f
+        JOIN film_category fc
+        ON f.film_id = fc.film_id
+        JOIN category c 
+        ON fc.category_id = c.category_id
+        WHERE f.title LIKE %s AND f.release_year BETWEEN %s AND %s
+        GROUP BY f.film_id
+        HAVING FIND_IN_SET(%s, categories) > 0
+        ORDER BY f.title
+        LIMIT %s OFFSET %s     
                     """
 
 FILMS_BY_ACTOR = """
-
+                        
+                
                  """
